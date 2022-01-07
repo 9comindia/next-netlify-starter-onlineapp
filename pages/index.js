7f76d2db01b28e3a42cb5c6a06b9f8bdbf3c8c32
9comindia <67412987+9comindia@users.noreply.github.com>
2022-01-07 17:47:41 +0530

    Update index.js

--- pages/index.js
+++ pages/index.js
@@ -6,14 +6,14 @@ export default function Home() {
   return (
     <div className="container">
       <Head>
-        <title>Next.js Starter!</title>
+        <title>Logic RACE app</title>
         <link rel="icon" href="/favicon.ico" />
       </Head>
 
       <main>
-        <Header title="Welcome to my app!" />
+        <Header title="Welcome to Logic RACE app!" />
         <p className="description">
-          Get started by editing <code>pages/index.js</code>
+          Get started by downloading from the Google Play store https://play.google.com/store/apps/details?id=com.logicrace.socialmediaplatform
         </p>
       </main>
 
