e6d5aa7a44817fb90470eb2130bba368be9f3c76
9comindia <67412987+9comindia@users.noreply.github.com>
2022-01-07 20:39:10 +0530

    Update index.js

--- pages/index.js
+++ pages/index.js
@@ -11,7 +11,7 @@ export default function Home() {
       </Head>
 
       <main>
-        <Header title="Welcome to Logic RACE app!" />
+        <Header title="!!Welcome to Logic RACE app!!" />
         <p>
     
     <a href="https://play.google.com/store/apps/details?id=com.logicrace.socialmediaplatform"><img src="/images/google-play-badge-telugu.png" alt="Logic RACE" /> </a>
